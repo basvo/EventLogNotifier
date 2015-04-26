7053dba40e46554f8456f94ce513b34a638426bd
basvo <ombergen@gmail.com>
2015-04-26 10:10:15 +0200

    Removed the GUI for the time being as it was just a mock

--- EventLogNotifier/NotifyIconViewModel.cs
+++ EventLogNotifier/NotifyIconViewModel.cs
@@ -11,41 +11,6 @@ namespace EventLogNotifier
     public class NotifyIconViewModel
     {
         /// <summary>
-        /// Shows a window, if none is already open.
-        /// </summary>
-        public ICommand ShowWindowCommand
-        {
-            get
-            {
-                return new DelegateCommand
-                {
-                    CanExecuteFunc = () => Application.Current.MainWindow == null,
-                    CommandAction = () =>
-                    {
-                        Application.Current.MainWindow = new MainWindow();
-                        Application.Current.MainWindow.Show();
-                    }
-                };
-            }
-        }
-
-        /// <summary>
-        /// Hides the main window. This command is only enabled if a window is open.
-        /// </summary>
-        public ICommand HideWindowCommand
-        {
-            get
-            {
-                return new DelegateCommand
-                {
-                    CommandAction = () => Application.Current.MainWindow.Close(),
-                    CanExecuteFunc = () => Application.Current.MainWindow != null
-                };
-            }
-        }
-
-
-        /// <summary>
         /// Shuts down the application.
         /// </summary>
         public ICommand ExitApplicationCommand
